0de06cd7c0a74b6ed3643e93b1be6be5692c6afe
Damir <Damir02@hotmail.nl>
2018-10-30 14:41:47 +0100

    af

--- horecaApp.js
+++ horecaApp.js
@@ -34,6 +34,11 @@ const prijsFris = 1.5;
 const prijsSmallSnack = 3.5;
 const prijsLargeSnack = 6.5;  
 
+
+
+addToOrder();
+
+function addToOrder(){
 	while ( input != 'stop'){
 	    var input = prompt('wat wilt u bestellen');
 			if (input == 'fris'){
@@ -46,33 +51,58 @@ const prijsLargeSnack = 6.5;
 				aantalWijn = aantalWijn + parseInt(prompt('hoeveel wijn wilt u bestellen'));
 			}
 			else if (input == 'snack'){
-				input = parseInt(prompt('Hoeveel bitterballen wilt u toevoegen (8 of 16)?'));
-			
-				if(input == '8'){
-					aantalSmallSnack = aantalSmallSnack + parseInt(prompt('Hoeveel bitterbalschalen van aantal stuks wilt u bestellen?'));
-				}
-				if(input == '16'){
-					aantalLargeSnack = aantalLargeSnack + parseInt(prompt('Hoeveel bitterbalschalen van aantal stuks wilt u bestellen?'));
-				}
+				addSnackToOrder();
 			}
 			else if (input == 'stop'){
 				prijs();
 			}
-			else{
+			else{	
 				alert('wij hebben geen ' + input + ' vandaag.')
-			}
+		}
+	}
+}
 
-	
- }
+
+function addSnackToOrder(){
+	// hier code om snack toe te voegen inclusief foutmelding anders dan 10 of 18
+	input = parseInt(prompt('Hoeveel bitterballen wilt u toevoegen (8 of 16)?'));
+			
+				if(input == '8'){
+					aantalSmallSnack = aantalSmallSnack + parseInt(prompt('Hoeveel bitterbalschalen van aantal stuks wilt u bestellen?'));
+				}
+				else if(input == '16'){
+					aantalLargeSnack = aantalLargeSnack + parseInt(prompt('Hoeveel bitterbalschalen van aantal stuks wilt u bestellen?'));
+				}
+				else{
+					alert('het getal ' + input + ' is ongeldig.');
+				}
+
+}
+
 
 function prijs(){
+	if(aantalFris >= 1){
+		document.write('fris: €' + prijsFris*aantalFris + ' aantal fris besteld ' + aantalFris + '<br>')
+}
+	if(aantalBier >= 1){
+		document.write('bier: €' + prijsBier*aantalBier + ' aantal bier besteld ' + aantalBier + '<br>')
+}
+	if(aantalWijn >= 1){		
+		document.write('wijn: €' + prijsWijn*aantalWijn + ' aantal wijn besteld ' + aantalWijn + '<br>')
+}
+	if(aantalSmallSnack >= 1){
+		document.write('snack: €' + prijsSmallSnack*aantalSmallSnack + ' aantal schalen met 8 bitterballen besteld ' + aantalSmallSnack + '<br>')
+}
+	if(aantalLargeSnack >= 1){
+		document.write('snack: €' + prijsLargeSnack*aantalLargeSnack + ' aantal schalen met 16 bitterballen besteld ' + aantalLargeSnack + '<br>')
+}	
+
+
+
+
+
 	var totaal = prijsFris*aantalFris + prijsBier*aantalBier + prijsWijn*aantalWijn + prijsSmallSnack*aantalSmallSnack + prijsLargeSnack*aantalLargeSnack
 
-	document.write('fris: €' + prijsFris*aantalFris + '<br>')
-	document.write('bier: €' + prijsBier*aantalBier + '<br>')
-	document.write('wijn: €' + prijsWijn*aantalWijn + '<br>')
-	document.write('snack: €' + prijsSmallSnack*aantalSmallSnack + '<br>')
-	document.write('snack: €' + prijsLargeSnack*aantalLargeSnack + '<br>')
 	document.write('totaal: €' + totaal)
 }
 
